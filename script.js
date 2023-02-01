28d596d7227eea51eaa9ca3b39d1761c3977a81e
Ionel Boicu <119396656+ionel96@users.noreply.github.com>
2023-02-01 13:48:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const snk =  document.getElementById("snake");
-snk.style.left = "213px";
-snk.style.top = "220px";
-const posFood = [];
+onload = (createGameBoard);
 let idUp = false, idDown = false, idLeft = false, idRight = false;
-let gameState = true;
+let gameState = true, fruitEaten = 0;
+let currentPos = [], linePos = 8, columnPos = 8;
+const gameBoard = Array.from(Array(16), () => new Array(16).fill(0));
 
 function createGameBoard() {
-    const board = document.getElementById("obj");
-    board.height = 15 * 25;
-    board.width = 17 * 25;  
-    const ctx = board.getContext('2d');
     let count = 0;
-    for (let i = 0; i < 17; ++i) {
-        for (let j = 0; j < 17; ++j) {
+    const box = document.getElementById('box');
+    for (let i = 0; i < 16; ++i) {
+        for (let j = 0; j < 16; ++j) {
+            const block = document.createElement('div');
+            block.id = i + " " + j;
+            block.style.width = '30px';
+            block.style.height = '30px';
+            block.classList.add('cell');
+            box.appendChild(block);
+            box.style.width = '30px';
             if (++count % 2) {
-                ctx.fillStyle = "rgba(77, 140, 45, 0.761)";
+                block.style.background = "rgba(77, 140, 45, 0.761)";
             } else {
-                ctx.fillStyle = "rgba(77, 149, 45, 0.761)";
+                block.style.background = "rgba(77, 149, 45, 0.761)";
             }
-            ctx.fillRect(j * 25, i * 25, 25, 25);
         }
+        ++count;
     }
-
-    for (let i = 45; i <= 395; i += 25) {
-        for (let j = 13; j <= 413; j += 25) {
-            posFood.push(i, j);
-        }
-    }
-    moveFood();
+    box.style.width = `${(30 * 16) + 2 * (16)}px`;
+    document.getElementById(linePos + " " + columnPos).innerHTML = "🟦";
+    currentPos.unshift(8, 8);
+    gameBoard[linePos][columnPos] = 1;
 }
 
 function getKeyAndMove(e) {
@@ -51,69 +51,92 @@ function getKeyAndMove(e) {
 }
 
 function moveLeft() {
-    if (!idRight) {
+    if (!idRight && !idLeft) {
         idLeft = setInterval(function() {
-            checkPosition(snk.offsetTop, snk.offsetLeft);
-            snk.style.left = parseInt(snk.style.left) - 25 + "px";
-        },400);
-        clearInterval(idUp);
-        clearInterval(idDown);
+            currentPos.unshift(linePos, --columnPos);
+            checkPosition(linePos, columnPos);
+        },200);
+        deleteRange(idUp, idDown);
         idUp = false;
         idDown = false;
     }
 }
 function moveUp() {
-    if (!idDown) {
+    if (!idDown && !idUp) {
         idUp = setInterval(function() {
-            checkPosition(snk.offsetTop, snk.offsetLeft);
-            snk.style.top = parseInt(snk.style.top) - 25 + "px";
-        },400);
-        clearInterval(idRight);
-        clearInterval(idLeft);
+            currentPos.unshift(--linePos, columnPos);
+            checkPosition(linePos, columnPos);
+        },200);
+        deleteRange(idRight, idLeft);
         idLeft = false;
         idRight = false;
     }
 }
 function moveRight() {
-    if (!idLeft) {
+    if (!idLeft && !idRight) {
         idRight = setInterval(function() {
-            checkPosition(snk.offsetTop, snk.offsetLeft);
-            snk.style.left = parseInt(snk.style.left) + 25 + "px";
-        },400);
-        clearInterval(idUp);
-        clearInterval(idDown);
+            currentPos.unshift(linePos, ++columnPos);
+            checkPosition(linePos, columnPos);
+        },200);
+        deleteRange(idUp, idDown);
         idUp = false;
         idDown = false;
     }
 }
 function moveDown() {
-    if (!idUp) {
+    if (!idUp && !idDown) {
         idDown = setInterval(function() {
-            checkPosition(snk.offsetTop, snk.offsetLeft);
-            snk.style.top = parseInt(snk.style.top) + 25 + "px";
-        },400);
-        clearInterval(idLeft);
-        clearInterval(idRight);
+            currentPos.unshift(++linePos, columnPos);
+            checkPosition(linePos, columnPos);
+        },200);
+        deleteRange(idLeft, idRight);
         idRight = false;
         idLeft = false;
     }
 }
 
 function checkPosition(posX, posY) {
-    if (posX == 20 || posX == 420 || posY == -12 || posY == 438) {
+    if (posX < 0 || posX > 15 || posY < 0 || posY > 15 || gameBoard[linePos][columnPos]) {
         gameState = false;
-        document.getElementById('tx').innerHTML = "Game over";
-        clearAllInterval();
+        deleteRange(idUp, idDown);
+        deleteRange(idLeft, idRight);
+        document.getElementById('output').innerHTML = "Game Over";
+        return;
+    } else {
+        document.getElementById(currentPos[0] + " " + currentPos[1]).innerHTML = "🟦";
+        gameBoard[currentPos[0]][currentPos[1]] = 1;
     }
+
     if (posX == parseInt(window.x) && posY == parseInt(window.y)) {
+        document.getElementById('counter').innerHTML = ++fruitEaten;
         moveFood();
+    } else {
+        deleteItems();
     }
 }
 
 function moveFood() {
-    let food = document.getElementById("food");
-    food.style.top = posFood[Math.floor(Math.random() * 510 / 2 ) * 2] + "px";
-    food.style.left = posFood[Math.floor(Math.random() * 510 / 2 ) * 2 - 1] + "px";
-    window.x = food.style.top;
-    window.y = food.style.left;
+    for (let j = 0; j < 5; ++j) {
+        let line = Math.floor(Math.random() * 15);
+        let colum = Math.floor(Math.random() * 15);
+        if (gameBoard[line][colum] == 1) {
+            continue;
+        }
+        document.getElementById(line + " " + colum).innerHTML = "🍎";
+        window.x = line;
+        window.y = colum;
+        break;
+    }
+}
+
+function deleteItems() {
+    document.getElementById(currentPos[currentPos.length - 2] + " " + currentPos[currentPos.length - 1]).innerHTML = "";
+    gameBoard[currentPos[currentPos.length - 2]][currentPos[currentPos.length - 1]] = 0;
+    currentPos.pop();
+    currentPos.pop();
+}
+
+function deleteRange(firstInterval, secInterval) {
+    clearInterval(firstInterval);
+    clearInterval(secInterval);
 }
